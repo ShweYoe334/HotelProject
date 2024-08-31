bb1cf6faeab7ee27db9dfd00f9acabb8afbdd087
ShweYoe334 <syoe3783@gmail.com>
2024-08-31 11:07:20 +0630

    Hotel

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust paths based on your project structure
+  ],
+  theme: {
+    screens:{
+      'sm' : '640px' ,
+      'md' : '768px' ,
+      'lg' :  '1250px' ,
+      'xl' :  '1280px' ,
+      '2xl' : 	 '1536px' ,
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+
